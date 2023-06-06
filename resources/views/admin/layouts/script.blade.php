3b405d90ae95030c1989210a82b6df3c1ad9c6df
ilyasDev <m.ilyas@codingpixel.com>
2023-06-06 23:38:25 +0500

    admin layout and dashboard

--- resources/views/admin/layouts/script.blade.php
+++ resources/views/admin/layouts/script.blade.php
@@ -17,3 +17,4 @@
 
  <script src="{{ asset('assets/vendor/datatables/jquery.dataTables.min.js') }}"></script>
  <script src="{{ asset('assets/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+ <script src="{{ asset('assets/js/demo/datatables-demo.js') }}"></script>
